8997f3b62b55c8f6b4b9f0169ccbd8d6cdffbe8f
prasanth.aj <santhtce@gmail.com>
2013-09-28 23:02:19 +0530

    changed the template

--- AvrCodeGenerator/CodeGenerator/Resources/usart_serial_def.h
+++ AvrCodeGenerator/CodeGenerator/Resources/usart_serial_def.h
@@ -1,17 +1,7 @@
-﻿#ifndef _CONF_USART_OPTIONS_H
-#define _CONF_USART_OPTIONS_H
-
-#include "conf_board.h"
+﻿#include "conf_board.h"
 #include "sysclk.h"
-
-/*! \name Configuration
- */
-//! @{
 #define $UsartName$_SERIAL_NAME           &$UsartName$
 #define $UsartName$_SERIAL_BAUDRATE		  $BaudRate$
 #define $UsartName$_SERIAL_CHAR_LENGTH    $UsartCharSize$
 #define $UsartName$_SERIAL_PARITY         $UsartParityMode$
 #define $UsartName$_SERIAL_STOP_BIT       $UsartStopBit$
-//! @}
-
-#endif // _CONF_USART_EXAMPLE_H
