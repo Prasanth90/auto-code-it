208cf49f78fd7b0ca7eb4c98af43feefc4577b99
prasanth.aj <santhtce@gmail.com>
2013-10-09 23:06:38 +0530

    Changed Treeview Header names

--- AvrCodeGenerator/DataModel/PeripheralNames.cs
+++ AvrCodeGenerator/DataModel/PeripheralNames.cs
@@ -8,8 +8,8 @@ namespace DataModel
     public static class PeripheralNames
     {
         public const string Port = "PORTS";
-        public const string ADC = "ADCs";
-        public const string DAC = "DACs";
+        public const string ADC = "ADCS";
+        public const string DAC = "DACS";
         public const string Timers = "TIMERS";
         public const string Usarts = "USARTS";
         public const string Spis = "SPIS";
