0b678f1a0f9a01545473ca66895756ef9e1f388f
prasanth.aj <santhtce@gmail.com>
2013-10-12 10:16:54 +0530

    Refactored code based on Plugins

---
+++ AvrCodeGenerator/Perpheral Config/CodeWizardPlugins/Timer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataModel;
+
+namespace PeripheralConfig.CodeWizardPlugins
+{
+    class Timer : ICodeWizardPlugin
+    {
+    }
+}
